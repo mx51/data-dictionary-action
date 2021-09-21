f0b52000292ef8659f9ed1d24e035432cdf0321a
Mike Gouline <1960272+gouline@users.noreply.github.com>
2021-09-21 13:04:03 +1000

    Refactor and make validation comments less noisy (#2)

--- action/command/validate.py
+++ action/command/validate.py
@@ -1,14 +1,6 @@
 import logging
 import json
-import jsoncfg
-from jsoncfg.config_classes import (
-    ConfigNode,
-    ConfigJSONObject,
-    ConfigJSONArray,
-    ConfigJSONScalar,
-)
 import requests
-from typing import Any
 from .base import Command
 
 
@@ -18,75 +10,61 @@ class Validate(Command):
         workspace: str,
         github_repository: str,
         github_token: str,
-        github_commit: str,
         github_pull: str,
+        github_commit: str,
     ):
         super().__init__(workspace)
         self.github_repository = github_repository
         self.github_token = github_token
-        self.github_commit = github_commit
         self.github_pull = github_pull
-        self.github_path = "data.json"
+        self.github_commit = github_commit
 
     def execute(self):
-        data = jsoncfg.load_config(str(self.data_path))
-        data, _ = self._augment(data)
+        errors = []
 
-        valid = True
+        data = self.read_data()
+        for store in data.get("stores", []):
+            if not store or "name" not in store:
+                logging.warning("Found invalid store: %s", store)
+                continue
 
-        stores, stores_line = data.get("stores", (None, None))
-        if stores:
-            for store, store_line in stores:
-                if not store:
-                    self._error("Empty store.", stores_line)
+            store_path = store["name"]
+
+            for table in store.get("tables", []):
+                if not table or "name" not in table:
+                    logging.warning("Found invalid table: %s", table)
                     continue
 
-                valid &= self._validate_description(store, store_line)
+                table_path = f"{store_path}.{table['name']}"
 
-                tables, tables_line = store.get("tables", (None, None))
-                if tables:
-                    for table, table_line in tables:
-                        if not table:
-                            self._error("Empty table.", tables_line)
-                            continue
+                if not table.get("description"):
+                    error = f"Missing description in table `{table_path}`"
+                    logging.error(error)
+                    errors.append(error)
 
-                        valid &= self._validate_description(table, table_line)
+                for field in table.get("fields", []):
+                    if not field or "name" not in field:
+                        logging.warning("Found invalid field: %s", field)
+                        continue
 
-                        fields, fields_line = table.get("fields", (None, None))
-                        if fields:
-                            for field, field_line in fields:
-                                if not field:
-                                    self._error("Empty field.", fields_line)
-                                    continue
+                    field_path = f"{table_path}.{field['name']}"
 
-                                valid &= self._validate_description(field, field_line)
+                    if not table.get("description"):
+                        error = f"Missing description in field `{field_path}`"
+                        logging.error(error)
+                        errors.append(error)
 
-        return valid
-
-    def _validate_description(self, item: dict, line: int) -> bool:
-        description, description_line = item.get("description", (None, None))
-        line = description_line if description_line else line
-
-        if not description:
-            self._error("Missing description.", line)
-            return False
-
-        return True
-
-    def _error(self, message: str, line: int):
-        logging.critical("L%s: %s", line, message)
-
-        self._github_request(
-            method="post",
-            context=f"pulls/{self.github_pull}/comments",
-            data={
-                "body": message,
-                "path": self.github_path,
-                "side": "RIGHT",
-                "line": line,
-                "commit_id": self.github_commit,
-            },
-        )
+        if errors:
+            self._github_request(
+                method="post",
+                context=f"issues/{self.github_pull}/comments",
+                data={
+                    "body": (
+                        f"### :no_entry_sign: Validation errors in `data.json` {self.github_commit}\n\n"
+                        + "\n".join([f"- {error}" for error in errors])
+                    ),
+                },
+            )
 
     def _github_request(self, method: str, context: str, data: dict):
         r = requests.request(
@@ -100,21 +78,3 @@ class Validate(Command):
         )
         if not r.ok:
             logging.error("Failed to create pull comment: %s", r)
-
-    @staticmethod
-    def _augment(element: ConfigNode) -> Any:
-        line = jsoncfg.node_location(element).line
-        if isinstance(element, ConfigJSONObject):
-            output_dict = {}
-            for key, value in element:
-                output_dict[key] = Validate._augment(value)
-            return (output_dict, line)
-        elif isinstance(element, ConfigJSONArray):
-            output_list = []
-            for item in element:
-                output_list.append(Validate._augment(item))
-            return (output_list, line)
-        elif isinstance(element, ConfigJSONScalar):
-            return (element(), line)
-        else:
-            raise ValueError(f"Unknown element type: {element}")
